c76b11975a4e8c06ffd47475bad84cfde41c2dbd
Jmkap <104119823+Jmk4p@users.noreply.github.com>
2025-02-03 22:43:40 +0800

    Added the server IP to the strings.xml. Now uses the server ip string in main activity; theoretically no need to run rasa anymore for as long as the server is up

--- app/src/main/java/com/thesis/periodtracker/MainActivity.java
+++ app/src/main/java/com/thesis/periodtracker/MainActivity.java
@@ -174,7 +174,7 @@ public class MainActivity extends AppCompatActivity {
         Gson gson = new GsonBuilder()
                 .registerTypeAdapter(RasaResponse.class, new RasaResponseDeserializer())
                 .create();
-        String computerIp = getString(R.string.rasa_server);
+        String computerIp = "192.168.254.102";
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://" + computerIp + ":5005/webhooks/rest/")
                 .client(okHttpClient)
