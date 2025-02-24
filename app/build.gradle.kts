0ab55d0ee7dadd488df92f3bfbdaf7536ea08757
nathbinalla <nathania.izabel@gmail.com>
2025-02-24 16:28:21 +0800

    added typing animation and message timeout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,5 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.15.1")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.15.1")
     implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("com.airbnb.android:lottie:6.1.0")
 }
